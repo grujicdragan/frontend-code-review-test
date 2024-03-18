00e8d00ea30e8ae86a0163a5cdd39c4f75a50b3f
dragan_grujic93@hotmail.com <dragan.grujic93@hotmail.com>
2024-03-18 14:47:58 +0100

    TASK-101101 Add comments for pull request task

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,27 @@
+// Remove all unused imports 
 import {Component, Injectable, Input, OnInit} from '@angular/core';
-import {NgClass, NgForOf, NgIf} from "@angular/common";
+import {NgClass, NgForOf, NgIf} from "@angular/common"; 
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 
+// Create separate folder for services and move this service there, also write unit tests for this service
 @Injectable()
 class MessageService {
   messages: Message[] = [];
 
-  async all() {
+  // Use dependency injection for HttpClient instead of using fetch
+  async all() { // Add a method return type
       const res = await fetch('http://127.0.0.1:4010/messages')
       const data = await res.json();
 
       this.messages = data.messages.map((message: any) => new Message(message.text, message.status));
   }
 
-  async add(message: Message) {
+  async add(message: Message) { // Add a method return type
     this.messages.push(message);
   }
 }
 
-class Message {
+class Message { // Create new folders 'models' and move this to separate file 'message.model.ts' 
   text;
   status: string;
   constructor(message: string, status: string) {
@@ -31,11 +34,13 @@ class Message {
   }
 }
 
+// Generate new angular MessageComponent component
 @Component({
-  selector: 'app-massage',
-  standalone: true,
+  selector: 'app-massage', // typo in selector - app-massage -> app-message
+  standalone: true, // Use @NgModule to define and import all modules, remove this
   template: `
     <div style="background-color: #fff;">
+    <!-- Remove text-slate-500 class since we are using in below in ngClass to add/remove it depending on message status  -->
       <span class="bg-slate-400" class="block bg-slate-200 text-slate-500">#{{no}} - {{ message.status }}</span>
       <div class="p-2" [ngClass]="{'text-slate-500': message.status === 'draft'}">
         {{message.text}}
@@ -47,16 +52,18 @@ class Message {
   ]
 })
 class MessageComponent {
-  @Input({ required: true }) message: any;
-  @Input() no: any;
+  @Input({ required: true }) message: any; // Don't use any type
+  @Input() no: any; // Use more descriptive names for class properties, also don't use any type
 }
 
+// Since there is no additional logic for chat component, only injecting MessageService to get an array of messages, 
+// should remove this component and inject MessageService into MessageComponent, remove Input() props there, and work with message array there
 @Component({
   selector: 'app-chat',
-  standalone: true,
-  providers: [MessageService],
+  standalone: true, // Use @NgModule to define and import all modules, remove this
+  providers: [MessageService], // Since we don't need own instance of MessageService for every instance of this component, remove this 
   imports: [
-    NgForOf,
+    NgForOf, // Remove unused imports 
     MessageComponent
   ],
   template: `
@@ -76,17 +83,24 @@ class ChatComponent implements OnInit {
     }
 
     async ngOnInit() {
+      // Remove ts-ignore annotations below
       // @ts-ignore
+
+      // Should refactor this method after changing MessageService to work with HttpClient and observables from RxJs
       await this.messageService.all();
-      this.messages = this.messageService.messages;
+      // It is a bad practice to access properties of service directly from other components (should be set to private).
+      // Also, to remain single source of truth principle, create a method to get messages
+      this.messages = this.messageService.messages; 
     }
 }
 
+// Generate new angular CreateMessageComponent component
 @Component({
   selector: 'app-create-message',
-  standalone: true,
-  providers: [MessageService],
+  standalone: true, // Use @NgModule to define and import all modules, remove this
+  providers: [MessageService], // // Since we don't need own instance of MessageService (Doing only http request) for every instance of this component, remove this 
   imports: [
+    // Remove unused imports 
     ReactiveFormsModule,
     FormsModule,
     MessageComponent,
@@ -94,7 +108,8 @@ class ChatComponent implements OnInit {
     NgClass,
   ],
   template: `
-    <div *ngIf="! message.empty()">
+  <!-- Remove blank space before message.empty() -->
+    <div *ngIf="! message.empty()"> 
       <app-massage [message]="message" no="preview"></app-massage>
     </div>
     <form (ngSubmit)="onSubmit()">
@@ -114,16 +129,17 @@ class ChatComponent implements OnInit {
 })
 class CreateMessageComponent {
   message: Message = new Message('', 'draft');
-  private messageService: MessageService;
+  private messageService: MessageService; // Remove this
 
-  constructor(messageService: MessageService) {
-    this.messageService = messageService;
+  constructor(messageService: MessageService) { // Add private access modifier and inject MessageService directly into constructor
+    this.messageService = messageService; // Remove this
   }
 
-  async onSubmit() {
+  // Should refactor this method after changing MessageService to work with HttpClient and observables from RxJs
+  async onSubmit() { // Add a method return type
       this.message.status = 'pending';
       const res = await fetch('http://127.0.0.1:4010/messages/send', {
-        method: 'GET',
+        method: 'GET', // Cannot send body in request for GET http methods
         body: JSON.stringify({text: this.message.text}),
       });
       res.status === 204 ? this.message.status = 'sent' : this.message.status = 'failed';
@@ -132,9 +148,10 @@ class CreateMessageComponent {
   }
 }
 
+// Generate new angular MessageComponent component
 @Component({
   selector: 'app-root',
-  standalone: true,
+  standalone: true, // Use @NgModule to define and import all modules, remove this
   imports: [
       ChatComponent,
       CreateMessageComponent
